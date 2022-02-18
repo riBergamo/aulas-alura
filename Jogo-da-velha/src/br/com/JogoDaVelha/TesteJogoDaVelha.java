0ad0aba094462273dc59e8c0115fe5f85918a27b
Riane Bergamo <89801847+riBergamo@users.noreply.github.com>
2022-02-18 15:37:47 -0300

    Delete Jogo-da-velha directory

--- Jogo-da-velha/src/br/com/JogoDaVelha/TesteJogoDaVelha.java
+++
@@ -1,9 +0,0 @@
-package br.com.JogoDaVelha;
-
-public class TesteJogoDaVelha {
-    public static void main(String[] args) {
-        Jogo jogo = new Jogo();
-        /*Tabuleiro tabuleiro = new Tabuleiro();
-        tabuleiro.exibeTabuleiro();*/
-    }
-}
